18b1745fc6ded50b1aa35dd10c61cab41c8d4864
胡 陈 <ChenHuSama@outlook.com>
2019-09-23 23:13:05 +0800

    feat : 新增演示ppt

--- App/Startup.cs
+++ App/Startup.cs
@@ -32,6 +32,16 @@ namespace App {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public IServiceProvider ConfigureServices(IServiceCollection services) {
+            ////每次GetService都会获得一个新的实例
+            //services.AddTransient<IDemoService,DemoService>();
+
+            ////在同一个Scope内只初始化一个实例 ，可以理解为（ 每一个request级别只创建一个实例，同一个http request会在一个 scope内）
+            //services.AddScoped<IDemoService, DemoService>();
+
+            ////整个应用程序生命周期以内只创建一个实例 
+            //services.AddSingleton<IDemoService, DemoService>();
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddControllersAsServices();
             ContainerBuilder builder = new ContainerBuilder();
             //新模块组件注册
